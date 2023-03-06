856236b59f00eca671ed5e571e93f8c5865f2934
Kay Kelly <kaykelly88@gmail.com>
2023-03-06 22:18:43 +1300

    Created a sketch page, where the user inputs a value which creates a grid where the mouse can be used to draw

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-for (i=0; i<256; i++) {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
-}
+const btn = document.querySelector('.setGrid');
+btn.addEventListener('click', ()=> {
+    let gridSize = parseInt(prompt('Enter the size of the grid (1-100)'));
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+            alert('Please enter a number between 1 and 100');
+            return;
+    }
 
-const divs = document.querySelectorAll('.grid');
-divs.forEach(div => {
-    div.addEventListener('mouseenter', ()=> {
-        div.style.backgroundColor = 'blue';
-    });
-    div.addEventListener('mouseleave', ()=> {
-        div.style.backgroundColor = 'white';
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    
+    for (let i=0; i<gridSize * gridSize; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+        div.addEventListener('mouseenter', ()=> {
+            div.style.backgroundColor = 'blue';
+        })};
+    
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => {
+        div.style.width = `${100 / gridSize}%`;
+        div.style.height = `${100 / gridSize}%`;
+        div.style.display = 'flex';
+        div.style.justifyContent = 'center';
+        div.style.alignItems = 'center';
     })
 })
+    
+
 
